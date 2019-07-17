db939cc4e70c86dac6ad005b9d577e04a0a05d6a
Seth Bridges <matvmp7@gmail.com>
2019-07-17 17:52:21 -0400

    started login feature

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,7 +28,11 @@ namespace team8.Controllers
 
             return View();
         }
-
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Login goes here";
+            return View();
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
